c8e9195902056c180b55745207e5f2170288ae07
Imtiaz Ahmed <imtiazahmedk@gmail.com>
2019-10-12 16:12:33 -0400

    Implemented the changes as per Professor's comments

--- Client.py
+++ Client.py
@@ -1,5 +1,6 @@
 import socket               # Import socket module
 
+inputString = input("Enter the input string: ")
 s = socket.socket()         # Create a socket object
 host = socket.gethostname() # Get local machine name
 port = 9500                # Reserve a port for your service.
@@ -7,8 +8,9 @@ port = 9500                # Reserve a port for your service.
 s.connect((host, port))
 
 # When client sends "Hello" message, the server send back "Hi"
-st='Hello'
-byt=st.encode()
+# When client sends "Test" message which is not "Hello", the server should send back "Goodbye"
+
+byt=inputString.encode()
 s.send(byt)
 print(s.recv(1024).decode())
 
